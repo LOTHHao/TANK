1397bd58add3ccf30c1d980da3162434b5025f12
LOTHHao <127651154+LOTHHao@users.noreply.github.com>
2023-09-27 22:55:17 +0700

    fix 2

--- Assets/Scripts/Tank/TankMovement.cs
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -4,21 +4,21 @@ namespace Complete
 {
     public class TankMovement : MonoBehaviour
     {
-        public int m_PlayerNumber = 1;              // Used to identify which tank belongs to which player.  This is set by this tank's manager.
-        public float m_Speed = 12f;                 // How fast the tank moves forward and back.
-        public float m_TurnSpeed = 180f;            // How fast the tank turns in degrees per second.
-        public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
-        public AudioClip m_EngineIdling;            // Audio to play when the tank isn't moving.
-        public AudioClip m_EngineDriving;           // Audio to play when the tank is moving.
-		public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
+        public int m_PlayerNumber = 1;              // Định danh người chơi 
+        public float m_Speed = 12f;                 // Vận tốc
+        public float m_TurnSpeed = 180f;            // Tốc độ cua
+        public AudioSource m_MovementAudio;         // Tham chiếu tới nguồn âm thanh di chuyển
+        public AudioClip m_EngineIdling;            // Âm thanh khi tank đứng tại chỗ 
+        public AudioClip m_EngineDriving;           // Âm thanh khi tank chạy
+		public float m_PitchRange = 0.2f;           // Mức độ ồn của động cơ
 
-        private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
-        private string m_TurnAxisName;              // The name of the input axis for turning.
-        private Rigidbody m_Rigidbody;              // Reference used to move the tank.
-        private float m_MovementInputValue;         // The current value of the movement input.
-        private float m_TurnInputValue;             // The current value of the turn input.
-        private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
-        private ParticleSystem[] m_particleSystems; // References to all the particles systems used by the Tanks
+        private string m_MovementAxisName;          // Nút di chuyển lên xuống
+        private string m_TurnAxisName;              // Nút bo cua
+        private Rigidbody m_Rigidbody;              // Tham chiếu đến xe tank
+        private float m_MovementInputValue;         // Giá trị di chuyển lên xuống
+        private float m_TurnInputValue;             // Giá trị bo cua
+        private float m_OriginalPitch;              // Mức độ âm thanh khi bắt đầu
+        private ParticleSystem[] m_particleSystems; // Tham chiếu đến hệ thống khói khi chạy
 
         private void Awake ()
         {
@@ -28,16 +28,14 @@ namespace Complete
 
         private void OnEnable ()
         {
-            // When the tank is turned on, make sure it's not kinematic.
+            // Khi tank khởi tạo chắc chắn là nó đứng im
             m_Rigidbody.isKinematic = false;
 
-            // Also reset the input values.
+            // Đặt lại các giá trị di chuyển 
             m_MovementInputValue = 0f;
             m_TurnInputValue = 0f;
 
-            // We grab all the Particle systems child of that Tank to be able to Stop/Play them on Deactivate/Activate
-            // It is needed because we move the Tank when spawning it, and if the Particle System is playing while we do that
-            // it "think" it move from (0,0,0) to the spawn point, creating a huge trail of smoke
+            // Hệ thống khói của tank khi chạy 
             m_particleSystems = GetComponentsInChildren<ParticleSystem>();
             for (int i = 0; i < m_particleSystems.Length; ++i)
             {
@@ -48,10 +46,10 @@ namespace Complete
 
         private void OnDisable ()
         {
-            // When the tank is turned off, set it to kinematic so it stops moving.
+            // Khi xe tank tắt chuyển qua Kinematic để đứng im
             m_Rigidbody.isKinematic = true;
 
-            // Stop all particle system so it "reset" it's position to the actual one instead of thinking we moved when spawning
+            // Tắt và reset hệ thống khói khi chạy
             for(int i = 0; i < m_particleSystems.Length; ++i)
             {
                 m_particleSystems[i].Stop();
@@ -61,18 +59,18 @@ namespace Complete
 
         private void Start ()
         {
-            // The axes names are based on player number.
+            // Nút bấm theo từng người chơi
             m_MovementAxisName = "Vertical" + m_PlayerNumber;
             m_TurnAxisName = "Horizontal" + m_PlayerNumber;
 
-            // Store the original pitch of the audio source.
+            // Lưu trữ âm thanh lúc đầu 
             m_OriginalPitch = m_MovementAudio.pitch;
         }
 
 
         private void Update ()
         {
-            // Store the value of both input axes.
+            // lưu trữ nút nhấn của player 
             m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
             m_TurnInputValue = Input.GetAxis (m_TurnAxisName);
 
@@ -82,13 +80,12 @@ namespace Complete
 
         private void EngineAudio ()
         {
-            // If there is no input (the tank is stationary)...
+            // Nếu không nhấn gì Tank đứng im
             if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f)
             {
-                // ... and if the audio source is currently playing the driving clip...
+                // Nguồn âm thanh di chuyển = âm thanh đang di chuyển 
                 if (m_MovementAudio.clip == m_EngineDriving)
                 {
-                    // ... change the clip to idling and play it.
                     m_MovementAudio.clip = m_EngineIdling;
                     m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
                     m_MovementAudio.Play ();
@@ -96,10 +93,10 @@ namespace Complete
             }
             else
             {
-                // Otherwise if the tank is moving and if the idling clip is currently playing...
+                // Nếu tank đang di chuyển và âm thanh đứng im đang phát 
                 if (m_MovementAudio.clip == m_EngineIdling)
                 {
-                    // ... change the clip to driving and play.
+                    // đổi âm thanh và chạy 
                     m_MovementAudio.clip = m_EngineDriving;
                     m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
                     m_MovementAudio.Play();
@@ -110,7 +107,6 @@ namespace Complete
 
         private void FixedUpdate ()
         {
-            // Adjust the rigidbodies position and orientation in FixedUpdate.
             Move ();
             Turn ();
         }
@@ -118,23 +114,15 @@ namespace Complete
 
         private void Move ()
         {
-            // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
             Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
-
-            // Apply this movement to the rigidbody's position.
             m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
         }
 
 
         private void Turn ()
         {
-            // Determine the number of degrees to be turned based on the input, speed and time between frames.
             float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
-
-            // Make this into a rotation in the y axis.
             Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
-
-            // Apply this rotation to the rigidbody's rotation.
             m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
         }
     }
--- Assets/Scripts/UI/UIDirectionControl.cs
+++ Assets/Scripts/UI/UIDirectionControl.cs
@@ -4,13 +4,8 @@ namespace Complete
 {
     public class UIDirectionControl : MonoBehaviour
     {
-        // This class is used to make sure world space UI
-        // elements such as the health bar face the correct direction.
-
-        public bool m_UseRelativeRotation = true;       // Use relative rotation should be used for this gameobject?
-
-
-        private Quaternion m_RelativeRotation;          // The local rotatation at the start of the scene.
+        public bool m_UseRelativeRotation = true;       
+        private Quaternion m_RelativeRotation;         
 
 
         private void Start ()
