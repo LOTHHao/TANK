70d1b908d0ac20996823953e37c45c4ea7ff1966
LOTHHao <127651154+LOTHHao@users.noreply.github.com>
2023-09-27 21:42:36 +0700

    Fix

--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -5,27 +5,27 @@ namespace Complete
 {
     public class TankShooting : MonoBehaviour
     {
-        public int m_PlayerNumber = 1;              
-        public Rigidbody m_Shell;                   
-        public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
-        public Slider m_AimSlider;                  // A child of the tank that displays the current launch force.
-        public AudioSource m_ShootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source.
-        public AudioClip m_ChargingClip;            // Audio that plays when each shot is charging up.
-        public AudioClip m_FireClip;                // Audio that plays when each shot is fired.
-        public float m_MinLaunchForce = 15f;        // The force given to the shell if the fire button is not held.
-        public float m_MaxLaunchForce = 30f;        // The force given to the shell if the fire button is held for the max charge time.
-        public float m_MaxChargeTime = 0.75f;       // How long the shell can charge for before it is fired at max force.
+        public int m_PlayerNumber = 1;              // Định danh player
+        public Rigidbody m_Shell;                   // Prefab đạn
+        public Transform m_FireTransform;           // Vị trí bắn đạn
+        public Slider m_AimSlider;                  // Hiển thị tầm bắn 
+        public AudioSource m_ShootingAudio;         // Truy cứu tới dữ liệu âm thanh 
+        public AudioClip m_ChargingClip;            // Âm thanh khi gồng bắn
+        public AudioClip m_FireClip;                // Âm thanh mỗi lần bắn 
+        public float m_MinLaunchForce = 15f;        // Lực gồng bắn tối thiểu
+        public float m_MaxLaunchForce = 30f;        // Lực gồng bắn tối đa
+        public float m_MaxChargeTime = 0.75f;       // Thời gian gồng tối đa
 
 
-        private string m_FireButton;                // The input axis that is used for launching shells.
-        private float m_CurrentLaunchForce;         // The force that will be given to the shell when the fire button is released.
-        private float m_ChargeSpeed;                // How fast the launch force increases, based on the max charge time.
-        private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
+        private string m_FireButton;                // Nút bắn
+        private float m_CurrentLaunchForce;         // Lực bắn hiện tại
+        private float m_ChargeSpeed;                // Tốc độ gồng
+        private bool m_Fired;                       // Check đạn đã được bắn hay chưa
 
 
         private void OnEnable()
         {
-            // When the tank is turned on, reset the launch force and the UI
+            // Khi xe tank được bật chỉnh lực và thanh canh chỉnh về min
             m_CurrentLaunchForce = m_MinLaunchForce;
             m_AimSlider.value = m_MinLaunchForce;
         }
@@ -33,49 +33,48 @@ namespace Complete
 
         private void Start ()
         {
-            // The fire axis is based on the player number.
+            // Nút bắn trên mỗi Player
             m_FireButton = "Fire" + m_PlayerNumber;
 
-            // The rate that the launch force charges up is the range of possible forces by the max charge time.
+            // Tốc độ lực phóng tăng lên và phạm vi lực có theo thời gian phóng tối đa.
             m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
         }
 
 
         private void Update ()
         {
-            // The slider should have a default value of the minimum launch force.
+            // Thanh canh chỉnh có giá trị tối thiểu 
             m_AimSlider.value = m_MinLaunchForce;
 
-            // If the max force has been exceeded and the shell hasn't yet been launched...
+            // Nếu giá trị gồng đã max và chưa bắn 
             if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
             {
-                // ... use the max force and launch the shell.
+                // Xử dụng giá trị gồng lớn nhất 
                 m_CurrentLaunchForce = m_MaxLaunchForce;
                 Fire ();
             }
-            // Otherwise, if the fire button has just started being pressed...
+            // Khi nút bắn được nhấn
             else if (Input.GetButtonDown (m_FireButton))
             {
-                // ... reset the fired flag and reset the launch force.
+                // khởi tạo lại bool bắn và lực bắn
                 m_Fired = false;
                 m_CurrentLaunchForce = m_MinLaunchForce;
 
-                // Change the clip to the charging clip and start it playing.
+                // Chuyển âm thanh bắn sang âm thanh gồng và chạy nó
                 m_ShootingAudio.clip = m_ChargingClip;
                 m_ShootingAudio.Play ();
             }
-            // Otherwise, if the fire button is being held and the shell hasn't been launched yet...
+            // Nếu nút bắn dược giữ và chưa bắn 
             else if (Input.GetButton (m_FireButton) && !m_Fired)
             {
-                // Increment the launch force and update the slider.
+                // Tăng lực bắn và thanh canh chỉnh 
                 m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
 
                 m_AimSlider.value = m_CurrentLaunchForce;
             }
-            // Otherwise, if the fire button is released and the shell hasn't been launched yet...
+            // Nếu nút bắn dc thả và chưa bắn
             else if (Input.GetButtonUp (m_FireButton) && !m_Fired)
             {
-                // ... launch the shell.
                 Fire ();
             }
         }
@@ -83,21 +82,20 @@ namespace Complete
 
         private void Fire ()
         {
-            // Set the fired flag so only Fire is only called once.
+            // Để chắc chắn là bắn chỉ được gọi 1 lần
             m_Fired = true;
 
-            // Create an instance of the shell and store a reference to it's rigidbody.
-            Rigidbody shellInstance =
-                Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+            // Tạo đạn và tham chiếu nó đến rigibody
+            Rigidbody shellInstance = Instantiate (m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
 
-            // Set the shell's velocity to the launch force in the fire position's forward direction.
+            // Đặt vận tốc bằng lực bắn về phía trước
             shellInstance.velocity = m_CurrentLaunchForce * m_FireTransform.forward; 
 
-            // Change the clip to the firing clip and play it.
+            // Đổi âm thanh bắn và chạy nó
             m_ShootingAudio.clip = m_FireClip;
             m_ShootingAudio.Play ();
 
-            // Reset the launch force.  This is a precaution in case of missing button events.
+            // Đặt lại lực bắn 
             m_CurrentLaunchForce = m_MinLaunchForce;
         }
     }
